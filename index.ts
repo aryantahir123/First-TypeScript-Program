7c75e1d66f299271a608c47347c8de7759e5ffde
aryantahit123 <aryanktk377@gmail.com>
2025-07-23 16:31:37 +0500

    done hogaya

--- index.ts
+++
@@ -1,8 +0,0 @@
-let data : [string,number,boolean] = ["Aryan",19,true];
-let arr1: string[]=["Salman","Umair","Khan"];
-let arr2: number[]=[1,2,3,4,5,6];
-let obj:{firstName:string, roll:number}={
-    firstName: "Nouman",
-    roll: 20
-}
-export{}
